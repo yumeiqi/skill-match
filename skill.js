409bb881720cf1549c336a9d46ac2d7d9b0937d4
quchen111 <2860871485@qq.com>
2016-06-29 15:34:38 +0800

    skills-match

---
+++ skill.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var skill = express();
+var mysql = require('mysql');
+
+var matchs = require('./db.json');
+// var db = mysql.createConnection({
+// 	host: 		'127.0.0.1',
+// 	user: 		'foodshop',
+// 	password: 	'123456',
+// 	database: 	'skills'
+// });
+
+var data = require('./lib/data');
+matchs.credentials.database = matchs.db;
+var db = mysql.createConnection(matchs.credentials);
+// logger.info ("cors is set to: " + matchs.cors);
+
+skill.get('/lib/data', function(req, res){
+  data.list(db, req, res);
+});
+
+skill.listen(3000);
